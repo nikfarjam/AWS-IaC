9693798f5a0526e347c23d3c4a59160ebd05638b
Mehdi <mm.nikfarjam@gmail.com>
2024-01-26 14:57:22 +1100

    Use networking module

--- main.tf
+++ main.tf
@@ -7,3 +7,13 @@ resource "aws_vpc" "diamond_dogs" {
     environment = var.environment
   }
 }
+
+module "networking" {
+  source           = "./networking"
+  aws_region       = var.aws_region
+  prefix           = var.prefix
+  environment      = var.environment
+  address_space    = var.address_space
+  public_cidrs     = "10.0.1.0/24"
+  private_cidrs    = "10.0.2.0/24"
+}
