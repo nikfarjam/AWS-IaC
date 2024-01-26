b9cd14b320155dfc514dff5af6db8c6e43b61f5b
Mehdi <mm.nikfarjam@gmail.com>
2024-01-26 14:58:14 +1100

    Remove dummy code from main

--- main.tf
+++ main.tf
@@ -1,13 +1,3 @@
-resource "aws_vpc" "diamond_dogs" {
-  cidr_block           = var.address_space
-  enable_dns_hostnames = true
-
-  tags = {
-    name        = "${var.prefix}-vpc-${var.aws_region}"
-    environment = var.environment
-  }
-}
-
 module "networking" {
   source           = "./networking"
   aws_region       = var.aws_region
