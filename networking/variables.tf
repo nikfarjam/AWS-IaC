784fc0399d4efb567b534a74f06578dd96f208be
Mehdi <mm.nikfarjam@gmail.com>
2024-01-26 14:40:51 +1100

    Add networking variables

--- networking/variables.tf
+++ networking/variables.tf
@@ -1,9 +1,27 @@
 variable "aws_region" {
   default     = "ap-southeast-2"
-  description = "(Optional) AWS region that resources are accosiated to"
+  description = "(Optional) AWS region that resources are associated to. Defaults to ap-southeast-2"
 }
 
 variable "prefix" {
   type        = string
-  description = "(Reguired) Prefix in the name of resources"
+  description = "(Required) Prefix in the name of resources"
+}
+
+variable "address_space" {
+  type        = string
+  description = "(Optional) The address space used by the virtual network. Defaults to 10.0.0.0/16."
+  default     = "10.0.0.0/16"
+}
+
+variable "public_cidrs" {
+  type        = string
+  description = "(Optional) The address space used by the public subnet. Defaults to 10.0.1.0/24."
+  default     = "10.0.1.0/24"
+}
+
+variable "private_cidrs" {
+  type        = string
+  description = "(Optional) The address space the private subnet uses. Defaults to 10.0.2.0/24."
+  default     = "10.0.2.0/24"
 }
