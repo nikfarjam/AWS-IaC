8843fdd2554694f9bda352503c0be743ac531cdc
Mehdi <mm.nikfarjam@gmail.com>
2024-01-28 09:18:20 +1100

    Remove prefix

--- main.tf
+++ main.tf
@@ -2,7 +2,6 @@ module "networking" {
   source           = "./networking"
   prefix           = var.prefix
   aws_region       = var.aws_region
-  prefix           = var.prefix
   environment      = var.environment
   address_space    = var.address_space
   public_cidrs     = "10.0.1.0/24"
