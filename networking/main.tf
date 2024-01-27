f4485a03fca119f3cfd120ceef6bb2ab0e033253
Mehdi <mm.nikfarjam@gmail.com>
2024-01-28 09:15:45 +1100

    Fix naming

--- networking/main.tf
+++ networking/main.tf
@@ -52,7 +52,7 @@ resource "aws_route_table" "tr_public_rt" {
 resource "aws_route" "tr_default_route" {
   route_table_id         = aws_route_table.tr_public_rt.id
   destination_cidr_block = "0.0.0.0/0"
-  gateway_id             = aws_internet_gateway.${var.prefix}_internet_gateway.id
+  gateway_id             = aws_internet_gateway.tr_internet_gateway.id
 }
 
 
