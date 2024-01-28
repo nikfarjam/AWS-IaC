ee7e423afd6b5e877a8ff609fb783a356e773f05
Mehdi <mm.nikfarjam@gmail.com>
2024-01-28 21:42:43 +1100

    Develop (#6) (#7)
    * Feature/ec2 (#4)
    
    * Add ec2 module
    
    * Add EC2 module
    
    * Fix network module (#5)

--- main.tf
+++ main.tf
@@ -7,3 +7,22 @@ module "networking" {
   public_cidrs  = "10.0.1.0/24"
   private_cidrs = "10.0.2.0/24"
 }
+
+locals {
+  public_security_groups = [
+    module.networking.vpc_security_group_http,
+    module.networking.vpc_security_group_https,
+    module.networking.vpc_security_group_ssh
+  ]
+}
+
+module "ec2" {
+  source                 = "./ec2"
+  prefix                 = var.prefix
+  aws_region             = var.aws_region
+  environment            = var.environment
+  instance_type          = "t2.micro"
+  subnet_id              = module.networking.public_subnet_group_name
+  vpc_security_group_ids = local.public_security_groups
+  vol_size               = 8
+}
---
+++ networking/outputs.tf
@@ -0,0 +1,23 @@
+output "vpc_id" {
+  value = aws_vpc.tr_vpc.id
+}
+
+output "private_subnet_group_name" {
+  value = aws_subnet.tr_private_subnet.id
+}
+
+output "public_subnet_group_name" {
+  value = aws_subnet.tr_public_subnet.id
+}
+
+output "vpc_security_group_http" {
+  value = aws_security_group.tr_security_group_http.id
+}
+
+output "vpc_security_group_https" {
+  value = aws_security_group.tr_security_group_https.id
+}
+
+output "vpc_security_group_ssh" {
+  value = aws_security_group.tr_security_group_ssh.id
+}
